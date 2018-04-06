7578d12f03ed1afa9907337fccca7a1307d93a18
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-04-06 10:48:29 -0500

    removed footer, optimized with constructor function

--- assets/app.js
+++ assets/app.js
@@ -1,244 +1,43 @@
 $(document).ready(function() {
-    let Q1 = {
-        q: "Madagascar is an island located off the southeast coast of what continent",
-        one: "South America",
-        two: "Asia",
-        three: "Australia",
-        four: "Africa",
-        correct: "Africa",
+
+    function Question(q, one, two, three, four, correct) {
+        this.q = q;
+        this.one = one;
+        this.two = two;
+        this.three = three;
+        this.four = four;
+        this.correct = correct;
     };
-    let Q2 = {
-        q: "What is the official language of Greenland?",
-        one: "Greenlandic",
-        two: "Danish",
-        three: "English",
-        four: "Aluu Inuugujoq Kutaa",
-        correct: "Greenlandic",
-    };
-    let Q3 = {
-        q: "In what country is the city of Turin?",
-        one: "France",
-        two: "Italy",
-        three: "Switzerland",
-        four: "Slovenia",
-        correct: "Italy",
-    };
-    let Q4 = {
-        q: "What is the capital city of Canada's Yukon territory?",
-        one: "Whitehorse",
-        two: "Calgary",
-        three: "Edmonton",
-        four: "Oxford",
-        correct: "Whitehorse",
-    };
-    let Q5 = {
-        q: "Macau is an autonomous territory belonging to which country?",
-        one: "Phillipines",
-        two: "China",
-        three: "India",
-        four: "Indonesia",
-        correct: "China",
-    };
-    let Q6 = {
-        q: "Which sea separates the East African coast and the Saudi Arabian peninsula?",
-        one: "Red Sea",
-        two: "Black Sea",
-        three: "Mediterranean Sea",
-        four: "Sea of Galilee",
-        correct: "Red Sea",
-    };
-    let Q7 = {
-        q: "What is the name for the monetary unit used in Thailand?",
-        one: "Pesos",
-        two: "Euro",
-        three: "Yen",
-        four: "Baht",
-        correct: "Baht",
-    };
-    let Q8 = {
-        q: "Hiragana, Katakana and Kanji are the names for writing systems used in which country?",
-        one: "China",
-        two: "Japan",
-        three: "Taiwan",
-        four: "India",
-        correct: "Japan",
-    };
-    let Q9 = {
-        q: "Chile shares the majority of its border with which other South American country?",
-        one: "Brazil",
-        two: "Peru",
-        three: "Argentina",
-        four: "Columbia",
-        correct: "Argentina",
-    };
-    let Q10 = {
-        q: "Alkebulan is the oldest indeginous name for which continent?",
-        one: "South America",
-        two: "North America",
-        three: "Africa",
-        four: "Australia",
-        correct: "Africa",
-    };
-    let Q11 = {
-        q: "Burkina Faso is a landlocked country located on which continent?",
-        one: "Asia",
-        two: "Australia",
-        three: "Africa",
-        four: "Europe",
-        correct: "Africa",
-    };
-    let Q12 = {
-        q: "What island state was formerly known by the name Formosa?",
-        one: "Taiwan",
-        two: "Macau",
-        three: "Phillipines",
-        four: "Madagascar",
-        correct: "Taiwan",
-    };
-    let Q13 = {
-        q: "The Principality of Monaco is a sovereign city-state bordered on three sides by which country?",
-        one: "Italy",
-        two: "France",
-        three: "Spain",
-        four: "Greece",
-        correct: "France",
-    };
-    let Q14 = {
-        q: "Suriname is a country located on which continent?",
-        one: "Africa",
-        two: "North America",
-        three: "South America",
-        four: "Europe",
-        correct: "South America",
-    };
-    let Q15 = {
-        q: "Wellington is the capital city of which island nation?",
-        one: "Australia",
-        two: "Scotland",
-        three: "Wales",
-        four: "New Zealand",
-        correct: "New Zealand",
-    };
-    let Q16 = {
-        q: "Atlantic City is a popular entertainment destination located in what U.S. state?",
-        one: "Virginia",
-        two: "New Jersey",
-        three: "North Carolina",
-        four: "New York",
-        correct: "New Jersey",
-    };
-    let Q17 = {
-        q: "What city in Australia has the highest population?",
-        one: "Sydney",
-        two: "Melbourne",
-        three: "Canberra",
-        four: "Adelaide",
-        correct: "Sydney",
-    };
-    let Q18 = {
-        q: "The avocado is a tree that is thought to have originated in what country?",
-        one: "Panama",
-        two: "Brazil",
-        three: "Mexico",
-        four: "Bermuda",
-        correct: "Mexico",
-    };
-    let Q19 = {
-        q: "In what country would you find the temple complex Angkor Wat?",
-        one: "Thailand",
-        two: "Vietnam",
-        three: "Burma",
-        four: "Cambodia",
-        correct: "Cambodia",
-    };
-    let Q20 = {
-        q: "The Hoover Dam in the United States is built on what river?",
-        one: "The Colorado River",
-        two: "The Mississippi River",
-        three: "The Nevada River",
-        four: "The Missouri River",
-        correct: "The Colorado River",
-    };
-    let Q21 = {
-        q: "San Marino is a microstate in Europe completely surrounded by what country?",
-        one: "France",
-        two: "Italy",
-        three: "Spain",
-        four: "Greece",
-        correct: "Italy",
-    };
-    let Q22 = {
-        q: "What is the Easternmost point on the North American continent?",
-        one: "Cape Columbia, Ellesmere Island",
-        two: "East Point Lighthouse, Prince Edward Island",
-        three: "Cape Spear, Newfoundland",
-        four: "Lubec, Maine",
-        correct: "Cape Spear, Newfoundland",
-    };
-    let Q23 = {
-        q: "Formerly known as Bedloe's Island, what is the current name of the island where the Statue of Liberty is located?",
-        one: "Liberty Island",
-        two: "Elles Island",
-        three: "New York Island",
-        four: "Hoffman Island",
-        correct: "Liberty Island",
-    };
-    let Q24 = {
-        q: "Sardinia, the second largest island in the Mediterranean Sea, is an autonomous region of what country?",
-        one: "Spain",
-        two: "Tunisia",
-        three: "Italy",
-        four: "Greece",
-        correct: "Italy",
-    };
-    let Q25 = {
-        q: "Located in Northwestern Turkey, which strait separates Europe and Asia?",
-        one: "Strait of Gibraltar",
-        two: "English Channel",
-        three: "Panama Canal",
-        four: "Bosphorus Strait",
-        correct: "Bosphorus Strait",
-    };
-    let Q26 = {
-        q: "South Africa completely surrounds which other African nation?",
-        one: "Namibia",
-        two: "Lesotho",
-        three: "Swaziland",
-        four: "Mozambique",
-        correct: "Lesotho",
-    };
-    let Q27 = {
-        q: "Europe is separated from Asia by which mountain range?",
-        one: "Ural Mountains",
-        two: "Himalayan Mountains",
-        three: "The Alps",
-        four: "The Pyrenees Mountains",
-        correct: "Ural Mountains",
-    };
-    let Q28 = {
-        q: "Which of the great lakes does not share a border with Canada?",
-        one: "Lake Michigan",
-        two: "Lake Superior",
-        three: "Lake Huron",
-        four: "Lake Ontario",
-        correct: "Lake Michigan",
-    };
-    let Q29 = {
-        q: "What is the national language of India?",
-        one: "Sanskrit",
-        two: "Hindi",
-        three: "Bengali",
-        four: "English",
-        correct: "Hindi",
-    };
-    let Q30 = {
-        q: "What is the largest island in the Caribbean Sea?",
-        one: "Jamaica",
-        two: "Haiti",
-        three: "Cuba",
-        four: "Dominican Republic",
-        correct: "Cuba",
-    };
+    let Q1 = new Question("Madagascar is an island located off the southeast coast of what continent", "South America", "Asia", "Australia", "Africa", "Africa");
+    let Q2 = new Question("What is the official language of Greenland?", "Greenlandic", "Danish", "English", "Aluu Inuugujoq Kutaa", "Greenlandic");
+    let Q3 = new Question("In what country is the city of Turin?", "France", "Italy", "Switzerland", "Slovenia", "Italy");
+    let Q4 = new Question("What is the capital city of Canada's Yukon territory?", "Whitehorse", "Calgary", "Edmonton", "Oxford", "Whitehorse");
+    let Q5 = new Question("Macau is an autonomous territory belonging to which country?", "Phillipines", "China", "India", "Indonesia", "China");
+    let Q6 = new Question("Which sea separates the East African coast and the Saudi Arabian peninsula?", "Red Sea", "Black Sea", "Mediterranean Sea", "Sea of Galilee", "Red Sea");
+    let Q7 = new Question("What is the name for the monetary unit used in Thailand?", "Pesos", "Euro", "Yen", "Baht", "Baht");
+    let Q8 = new Question("Hiragana, Katakana and Kanji are the names for writing systems used in which country?", "China", "Japan", "Taiwan", "India", "Japan");
+    let Q9 = new Question("Chile shares the majority of its border with which other South American country?", "Brazil", "Peru", "Argentina", "Columbia", "Argentina");
+    let Q10 = new Question("Alkebulan is the oldest indeginous name for which continent?", "South America", "North America", "Africa", "Australia", "Africa");
+    let Q11 = new Question("Burkina Faso is a landlocked country located on which continent?", "Asia", "Australia", "Africa", "Europe", "Africa");
+    let Q12 = new Question("What island state was formerly known by the name Formosa?", "Taiwan", "Madagascar", "Macau", "Phillipines", "Taiwan",);
+    let Q13 = new Question("The Principality of Monaco is a sovereign city-state bordered on three sides by which country?", "Italy", "France", "Spain", "Greece", "France");
+    let Q14 = new Question("Suriname is a country located on which continent?", "Africa", "North America", "South America", "Europe", "South America");
+    let Q15 = new Question("Wellington is the capital city of which island nation?", "Australia", "Scotland", "Wales", "New Zealand", "New Zealand");
+    let Q16 = new Question("Atlantic City is a popular entertainment destination located in what U.S. state?", "Virginia", "New Jersey", "North Carolina", "New York", "New Jersey");
+    let Q17 = new Question("What city in Australia has the highest population?", "Sydney", "Melbourne", "Canberra", "Adelaide", "Sydney");
+    let Q18 = new Question("The avocado is a tree that is thought to have originated in what country?", "Panama", "Brazil", "Mexico", "Bermuda", "Mexico");
+    let Q19 = new Question("In what country would you find the temple complex Angkor Wat?", "Thailand", "Vietnam", "Burma", "Cambodia", "Cambodia");
+    let Q20 = new Question("The Hoover Dam in the United States is built on what river?", "The Colorado River", "The Mississippi River", "The Nevada River", "The Missouri River", "The Colorado River");
+    let Q21 = new Question("San Marino is a microstate in Europe completely surrounded by what country?", "France", "Italy", "Spain", "Greece", "Italy");
+    let Q22 = new Question("What is the Easternmost point on the North American continent?", "Cape Columbia, Ellesmere Island", "East Point Lighthouse, Prince Edward Island", "Cape Spear, Newfoundland", "Lubec, Maine", "Cape Spear, Newfoundland");
+    let Q23 = new Question("Formerly known as Bedloe's Island, what is the current name of the island where the Statue of Liberty is located?", "Liberty Island", "Elles Island", "New York Island", "Hoffman Island", "Liberty Island");
+    let Q24 = new Question("Sardinia, the second largest island in the Mediterranean Sea, is an autonomous region of what country?", "Spain", "Tunisia", "Italy", "Greece", "Italy");
+    let Q25 = new Question("Located in Northwestern Turkey, which strait separates Europe and Asia?", "Strait of Gibraltar", "English Channel", "Panama Canal", "Bosphorus Strait", "Bosphorus Strait");
+    let Q26 = new Question("South Africa completely surrounds which other African nation?", "Namibia", "Lesotho", "Swaziland", "Mozambique", "Lesotho");
+    let Q27 = new Question("Europe is separated from Asia by which mountain range?", "Ural Mountains", "Himalayan Mountains", "The Alps", "The Pyrenees Mountains", "Ural Mountains");
+    let Q28 = new Question("Which of the great lakes does not share a border with Canada?", "Lake Michigan", "Lake Superior", "Lake Huron", "Lake Ontario", "Lake Michigan");
+    let Q29 = new Question("What is the national language of India?", "Sanskrit", "Hindi", "Bengali", "English", "Hindi");
+    let Q30 = new Question("What is the largest island in the Caribbean Sea?", "Jamaica", "Haiti", "Cuba", "Dominican Republic", "Cuba");
 
     let questionArray = [Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9, Q10, Q11, Q12, Q13, Q14, Q15, Q16, Q17, Q18, Q19, Q20, Q21, Q22, Q23, Q24, Q25, Q26, Q27, Q28, Q29, Q30];
     let correctAnswers = 0;
